bb55021591a2497bf456dc34197f63a99b1a0aea
Sumeyye Guler <sumeyyeguler0602@gmail.com>
2022-09-14 22:18:02 +0300

    14.09.2022

---
+++ src/test/java/day03_locators/C05_HomeWork2.java
@@ -0,0 +1,14 @@
+package day03_locators;
+
+public class C05_HomeWork2 {
+    public static void main(String[] args) {
+        /*
+        1. Bir class oluşturun : AmazonSearchTest
+        2. Main method oluşturun ve aşağıdaki görevi tamamlayın.
+        a.web sayfasına gidin. https://www.amazon.com/
+        b. Search(ara) “city bike”
+        c. Amazon'da görüntülenen ilgili sonuçların sayısını yazdırın
+        d. Sonra karşınıza çıkan ilk sonucun resmine tıklayın.
+         */
+    }
+}
